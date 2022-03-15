136bf7f86261c82254c8a89e37398cca95098c3b
Muslum Iskəndərov <62115636+muslumiskender@users.noreply.github.com>
2022-03-15 13:47:53 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { alphabet } from "./Data";
 
 function App() {
+  const [input, setInput] = useState();
+  const [data, setData] = useState([]);
+  const [number, setNumber] = useState();
+
+  const handleText = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleNumber = (e) => {
+    setNumber(parseInt(e.target.value));
+  };
+
+  let dataChanger;
+
+  let lastArr = [];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input) {
+      dataChanger = input.split("");
+
+      for (let i = 0; i < dataChanger.length; i++) {
+        let x;
+        if (dataChanger[i].match(/[^A-Za-z]/g)) {
+          x = dataChanger[i];
+        } else {
+          if (dataChanger[i] === dataChanger[i].toLowerCase()) {
+            if (
+              alphabet.indexOf(dataChanger[i].toUpperCase()) + number >
+              alphabet.length - 1
+            ) {
+              x =
+                alphabet[
+                  alphabet.indexOf(dataChanger[i].toUpperCase()) +
+                    number -
+                    alphabet.length
+                ].toLowerCase();
+            } else {
+              x =
+                alphabet[
+                  alphabet.indexOf(dataChanger[i].toUpperCase()) + number
+                ]?.toLowerCase();
+            }
+          } else {
+            if (
+              alphabet.indexOf(dataChanger[i]) + number >
+              alphabet.length - 1
+            ) {
+              x =
+                alphabet[
+                  alphabet.indexOf(dataChanger[i]) + number - alphabet.length
+                ];
+            } else {
+              x = alphabet[alphabet.indexOf(dataChanger[i]) + number];
+            }
+          }
+        }
+        lastArr.push(x);
+      }
+    }
+
+    lastArr.join("");
+    setData((x) => [...x, lastArr.join("")]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Sezar şifrələnməsi</h2>
+      <div className="main">
+        <form onSubmit={handleSubmit}>
+          <label>Mətni daxil edin:</label>
+          <textarea
+            type="text"
+            onChange={handleText}
+            value={input || ""}
+          ></textarea>
+          <label>Açarı seçin:</label>
+          <input
+            type="number"
+            onChange={handleNumber}
+            value={number || ""}
+            min="1"
+            max="25"
+          />
+          <button type="submit">Şifrələ</button>
+          {data.length > 0 ? (
+            <p>
+              Nəticə:{" "}
+              <div>
+                <i>{data[data.length - 1]}</i>
+              </div>
+            </p>
+          ) : (
+            ""
+          )}
+        </form>
+      </div>
     </div>
   );
 }
